c777cfcfe8c51124a100ec9021b9657501e1bc00
kungfooman <lama12345@gmail.com>
2019-01-27 19:55:52 +0100

    ignore config.ts changes

--- config.ts
+++
@@ -1,6 +0,0 @@
-export var mysql_host = "127.0.0.1";
-export var mysql_port = 3306;
-export var mysql_sock = "/var/run/mysqld/mysqld.sock";
-export var mysql_user = "gametracker";
-export var mysql_pass = "d78z78d32iorj";
-export var mysql_database = "gametracker";
