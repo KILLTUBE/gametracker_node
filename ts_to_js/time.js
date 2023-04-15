bba4e55284a7c7317b372439659dcb5e93b8021a
kungfooman <lama12345@gmail.com>
2023-04-15 15:35:57 +0200

    ts -> js

--- ts_to_js/time.js
+++ ts_to_js/time.js
@@ -1,6 +1,6 @@
-export function now() {
-    return new Date().valueOf();
+export function now(): number {
+  return new Date().valueOf();
 }
-export function time() {
-    return Math.round(now() / 1000);
+export function time(): number { // this is php-like for the database
+  return Math.round( now() / 1000 );
 }
--- ts_to_js/tracker.js
+++ ts_to_js/tracker.js
@@ -1,52 +1,55 @@
 import { State } from "./Globals";
+import { MysqlError } from "mysql";
 import { updateGameserver, updateAll, gameservers_init } from "./gameservers";
 import { deleteCrapServers, queryMasterserver, masterserver_init } from "./masterserver";
 import { mysql_sock, mysql_user, mysql_pass, mysql_database } from "./config";
 State.dgram = require("dgram");
 State.fs = require('fs');
 State.mysql = require("mysql").createConnection({
-    socketPath: mysql_sock,
-    user: mysql_user,
-    password: mysql_pass
+  //host: mysql_host,
+  //port: mysql_port,
+  socketPath: mysql_sock,
+  user: mysql_user,
+  password: mysql_pass
 });
-var fakeport = undefined;
+var fakeport = undefined; // make global
 var debug = undefined;
-State.mysql.query("USE " + mysql_database, function (err) {
-    main();
+State.mysql.query("USE " + mysql_database, function(err: MysqlError) {
+  main();
 });
 export function main() {
-    console.log("READY TO FIGHT");
-    gameservers_init();
-    masterserver_init();
-    debug = false;
-    if (debug) {
-        fakeport = 666;
-        updateGameserver("85.25.95.104", 28968);
-    }
-    else {
-        updateAll();
-        deleteCrapServers();
-        queryMasterserver();
-    }
+  console.log("READY TO FIGHT");
+  gameservers_init();
+  masterserver_init();
+  debug = false;
+  //debug = true;
+  if (debug)
+  {
+    fakeport = 666;
+    updateGameserver("85.25.95.104", 28968);
+  } else {
+    updateAll();
+    deleteCrapServers();
+    queryMasterserver();
+  }
 }
 export function repl() {
-    var buffer = '';
-    process.stdin.on('data', function (chunk) {
-        buffer += chunk.toString('utf8');
-    });
-    require('repl').start({
-        input: process.stdin,
-        output: process.stdout,
-        'eval': function (cmd, context, filename, callback) {
-            console.log(buffer);
-            try {
-                callback(null, eval(buffer));
-            }
-            catch (e) {
-                console.log(e);
-            }
-            buffer = '';
-        }
-    });
+  var buffer = '';
+  process.stdin.on('data', function(chunk) {
+    buffer += chunk.toString('utf8');
+  });
+  require('repl').start({
+    input: process.stdin,
+    output: process.stdout,
+    'eval': function (cmd: any, context: any, filename: any, callback: any) {
+      console.log(buffer);
+      try {
+        callback(null, eval(buffer));
+      } catch (e) {
+        console.log(e);
+      }
+      buffer = '';
+    }
+  });
 }
 repl();
